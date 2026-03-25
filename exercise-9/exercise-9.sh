c48270878cff3a3973d5a782d6c13fed0de964d4
Ben Cöppicus <ben.coeppicus@it-scholar.com>
2026-03-25 11:33:04 +0100

    Add CertificateSigningRequest and Role for developer Jane

--- exercise-9/exercise-9.sh
+++ exercise-9/exercise-9.sh
@@ -1,3 +1,10 @@
 openssl genrsa -out jane.key 2048
 openssl req -new -key jane.key -out jane.csr -subj "/CN=jane/O=developers"
 cat jane.csr | base64 | tr -d '\n'
+kubectl apply -f jane-csr.yaml
+kubectl certificate approve jane
+kubectl get csr jane -o jsonpath='{.status.certificate}' | base64 -d > jane.crt
+kubectl apply -f jane-role.yaml
+kubectl config set-credentials jane \
+  --client-certificate=jane.crt \
+  --client-key=jane.key
